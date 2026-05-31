df399b8f79ec2ffeded8318b20413cfed880baa0
sloumadis-netizen <sloumadis@gmail.com>
2026-05-31 03:46:18 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I am alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+keep_alive()
+bot.run(os.environ.get('TOKEN'))
